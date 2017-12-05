f53a7ab1081bb981405d6ad1ba4c12d45b5dfdb6
aksinghchauhan <34186307+aksinghchauhan@users.noreply.github.com>
2017-12-06 00:58:37 +0530

    Adding request parser

--- router.js
+++ router.js
@@ -3,9 +3,7 @@
 var logger = require('winston');
 var express = require("express");
 var config = require('config');
-//var deviceCtrl = require('./controllers/device/main');
 var bodyParser = require('body-parser');
-var fooApi = require('./controllers/api/backend');
 
 var vendorApi = require('./controllers/api/vendorws');
 var connectApi = require('./controllers/connect');
@@ -13,7 +11,14 @@ var connectApi = require('./controllers/connect');
 var app = module.exports = express();
 
 logger.debug("registering ui middleware");
-//app.get("/ui/devices", deviceCtrl.hello);
+
+app.use(bodyParser.json({
+	limit : '50mb'
+}));
+app.use(bodyParser.urlencoded({
+	limit : '50mb',
+	extended : true
+}));
 
 app.get("/sr/api/foo", fooApi.getFoo);
 app.get("/sr/api/login", fooApi.getLogin);
@@ -24,3 +29,4 @@ app.post("/sr/api/addDetails", fooApi.onAdd);
 
 app.get("/sr/api/getvendorlist", vendorApi.getvendorlist);
 app.get("/sr/api/getPersitedValue", connectApi.getPersitedValue);
+app.post("/app/vendor/registerVendor", connectApi.registerVendor);
